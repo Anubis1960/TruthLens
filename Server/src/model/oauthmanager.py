3203f480f21a1c51c0912032c9c35d2c20628a99
RobertIlea <robert.ilea03@e-uvt.ro>
2025-03-15 06:41:52 +0200

    commented lines

---
+++ Server/src/model/oauthmanager.py
@@ -0,0 +1,59 @@
+from authlib.integrations.flask_client import OAuth
+
+#######################
+#
+# OAuth 2.0 Config - GOOGLE
+#
+#######################
+class OAuthManager:
+    """
+    Manages OAuth 2.0 authentication with external providers, in this case, Google.
+
+    Attributes:
+        oauth (OAuth): The OAuth object to handle the OAuth authentication flow.
+    """
+
+    def __init__(self, app=None):
+        """
+        Initializes the OAuthManager object. Optionally initializes OAuth with the Flask app.
+
+        Args:
+            app (Flask, optional): A Flask app instance to initialize the OAuth object with. Defaults to None.
+        """
+        self.oauth = OAuth()
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """
+        Initializes OAuth with the app configuration and registers the Google OAuth provider.
+
+        Args:
+            app (Flask): A Flask app instance to configure OAuth.
+        """
+        self.oauth.init_app(app)
+        # self.oauth.register(
+        #     name='google',
+        #     client_id='860148270648-qpgt1p8lr8fbali6svnsgifooj7eddac.apps.googleusercontent.com',
+        #     client_secret='',
+        #     access_token_url='https://accounts.google.com/o/oauth2/token',
+        #     access_token_params=None,
+        #     authorize_url='https://accounts.google.com/o/oauth2/auth',
+        #     authorize_params=None,
+        #     api_base_url='https://www.googleapis.com/oauth2/v1/',
+        #     userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',
+        #     client_kwargs={'scope': 'email profile'},
+        #     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration'
+        # )
+
+    def get_provider(self, name):
+        """
+        Retrieves a registered OAuth provider by name.
+
+        Args:
+            name (str): The name of the OAuth provider.
+
+        Returns:
+            OAuthClient: The OAuth client associated with the provider.
+        """
+        return self.oauth.create_client(name)
