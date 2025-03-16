8e2a56d521bc079dc896a19387abe23b1958ab04
Anubis1960 <alexander.rk19@gmail.com>
2025-03-16 17:05:01 +0200

    new model

--- Server/src/util/img/img_detect.py
+++ Server/src/util/img/img_detect.py
@@ -3,6 +3,11 @@ import numpy as np
 from cv2.typing import MatLike
 import tensorflow as tf
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+m_path = os.getenv('IMG_MODEL')
 
 def load_modell():
     # Get the directory of the current script
@@ -12,16 +17,27 @@ def load_modell():
     model = tf.keras.models.load_model(model_path)
     return model
 
+def new_load_modell():
+    model = tf.keras.models.load_model(m_path)
+    return model
+
 def prepare_image(img: MatLike) -> MatLike:
     image = cv2.resize(img, (32, 32))
     image = image / 255
     image = np.expand_dims(image, axis=0)
     return image
 
+def new_prep_img(img: MatLike) -> MatLike:
+    image = cv2.resize(img, (64, 64))
+    image = image / 255
+    image = np.expand_dims(image, axis=0)
+    return image
+
 def predict_image(image: MatLike) -> int:
     print("Predicting")
-    model = load_modell()
-    prepared_image = prepare_image(image)
+    model = new_load_modell()
+    # prepared_image = prepare_image(image)
+    prepared_image = new_prep_img(image)
     return model.predict(prepared_image)
 
 if __name__ == "__main__":
